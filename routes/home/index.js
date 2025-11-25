9e8d227672c5b8c2da04bb7215dadaac36272058
NGUYỄN VIỆT HẬU <nguyenviethau.it.2004@gmail.com>
2025-11-25 17:06:41 +0700

    update lại file

--- routes/home/index.js
+++ routes/home/index.js
@@ -10,13 +10,5 @@ router.get("/", function (req, res, next) {
   });
 });
 
-/* GET about page. */
-router.get("/aboutus", function (req, res, next) {
-  res.render("about/index", {
-    title: "VUIDOKAN",
-    company: "Công ty TNHH Phát triển Thể thao Vuidokan",
-    page: "about",
-  });
-});
 
 module.exports = router;
